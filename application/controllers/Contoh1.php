b059cd18ff2a706e8081d69845c23269a35b9d8f
febriindra <fotompep@gmail.com>
2023-09-26 20:47:07 +0700

    Membuat project CI 3 Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index(){
+        echo"<h1> Perkenalkan </h1>";
+        echo"Nama Saya Febri Indra 
+        Saya Tinggal di daerah kembangan 
+        olahraga yang saya sukai adalah sepakbola";
+    }
+}
+
+
+
+?>
